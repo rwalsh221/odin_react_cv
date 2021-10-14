75752f0f416220c3905d6e9398e2b93a5560eb46
Richard Walsh <richwalsh2@googlemail.com>
2021-10-14 18:37:59 +0100

    eslint containers and remove unused containers css.

--- odin_cv/src/containers/Home/Home.js
+++ odin_cv/src/containers/Home/Home.js
@@ -3,13 +3,12 @@ import React from 'react';
 import HeaderNavigation from '../../components/HeaderNavigation/HeaderNavigation';
 import CVInput from '../../components/CVInput/CVInput';
 
-const Home = (props) => {
-  return (
-    <React.Fragment>
-      <HeaderNavigation home={true} />
-      <CVInput />
-    </React.Fragment>
-  );
-};
+const Home = () => (
+  <>
+    <HeaderNavigation home />
+    {/* <HeaderNavigation home={true} /> */}
+    <CVInput />
+  </>
+);
 
 export default Home;
--- odin_cv/src/containers/Preview/Preview.js
+++ odin_cv/src/containers/Preview/Preview.js
@@ -3,13 +3,12 @@ import React from 'react';
 import HeaderNavigation from '../../components/HeaderNavigation/HeaderNavigation';
 import CVPreview from '../../components/CVPreview/CVPreview';
 
-const Preview = (props) => {
-  return (
-    <React.Fragment>
-      <HeaderNavigation preview={true} />
-      <CVPreview />
-    </React.Fragment>
-  );
-};
+const Preview = () => (
+  <>
+    <HeaderNavigation preview />
+    {/* <HeaderNavigation preview={true} /> */}
+    <CVPreview />
+  </>
+);
 
 export default Preview;
